2dc84b112801a60614281f15ac069dac77dc1818
Mingxing Zhang <james0zan@gmail.com>
2012-12-06 23:52:44 +0800

    extent server

--- extent_server.cc
+++ extent_server.cc
@@ -8,37 +8,50 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-extent_server::extent_server() {}
+extent_server::extent_server() {
+  pthread_mutex_init(&lock, NULL);
+}
 
 
 int extent_server::put(extent_protocol::extentid_t id, std::string buf, int &)
 {
-  // You fill this in for Lab 2.
-  return extent_protocol::IOERR;
+  ScopedLock m(&lock);
+  file &ex = disk[id];
+  ex.str = buf;
+  
+  unsigned int t = time(NULL);
+  ex.attr.ctime = t; ex.attr.mtime = t;
+  ex.attr.size = buf.size();
+  return extent_protocol::OK;
 }
 
 int extent_server::get(extent_protocol::extentid_t id, std::string &buf)
 {
-  // You fill this in for Lab 2.
-  return extent_protocol::IOERR;
+  ScopedLock m(&lock);
+  std::map<extent_protocol::extentid_t, file>::iterator it = disk.find(id);
+  if (it == disk.end()) return extent_protocol::NOENT;
+  buf = it->second.str;
+  
+  unsigned int t = time(NULL);
+  it->second.attr.atime = t;
+  return extent_protocol::OK;
 }
 
 int extent_server::getattr(extent_protocol::extentid_t id, extent_protocol::attr &a)
 {
-  // You fill this in for Lab 2.
-  // You replace this with a real implementation. We send a phony response
-  // for now because it's difficult to get FUSE to do anything (including
-  // unmount) if getattr fails.
-  a.size = 0;
-  a.atime = 0;
-  a.mtime = 0;
-  a.ctime = 0;
+  ScopedLock m(&lock);
+  std::map<extent_protocol::extentid_t, file>::iterator it = disk.find(id);
+  if (it == disk.end()) return extent_protocol::NOENT;
+  a = it->second.attr;
   return extent_protocol::OK;
 }
 
 int extent_server::remove(extent_protocol::extentid_t id, int &)
 {
-  // You fill this in for Lab 2.
-  return extent_protocol::IOERR;
+  ScopedLock m(&lock);
+  std::map<extent_protocol::extentid_t, file>::iterator it = disk.find(id);
+  if (it == disk.end()) return extent_protocol::NOENT;
+  disk.erase(it);
+  return extent_protocol::OK;
 }
 
