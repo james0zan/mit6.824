7864b16316e05a828a7e53cb09a419870401ca4c
Mingxing Zhang <james0zan@gmail.com>
2012-12-06 21:31:05 +0800

    step one @ lab 1

--- lock_server.h
+++ lock_server.h
@@ -8,16 +8,23 @@
 #include "lock_protocol.h"
 #include "lock_client.h"
 #include "rpc.h"
+#include <map>
+//#define _DEBUG
 
 class lock_server {
 
  protected:
   int nacquire;
+  std::map<lock_protocol::lockid_t, int> possessed;
+  pthread_mutex_t mutex;
+  pthread_cond_t cond;
 
  public:
   lock_server();
   ~lock_server() {};
-  lock_protocol::status stat(int clt, lock_protocol::lockid_t lid, int &);
+  lock_protocol::status stat(int clt, lock_protocol::lockid_t lid, int &r);
+  lock_protocol::status acquire(int clt, lock_protocol::lockid_t lid, lock_protocol::status &r);
+  lock_protocol::status release(int clt, lock_protocol::lockid_t lid, lock_protocol::status &r);
 };
 
 #endif 
